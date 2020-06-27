b6ded86a07e3f1e5a04590954f54868e41b71745
Celina Jackson <celinanjackson@gmail.com>
2020-06-27 15:53:28 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -14,6 +14,8 @@ def hopper
           :known_for => "Unix",
           :languages => ["C"]
         }
+
+				programmer_hash [:grace_hopper]
      }
 
 
@@ -21,8 +23,9 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
-	programmer_hash = 
+	programmer_hash[:alan_kay][:known_for]
+
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -40,7 +43,7 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -63,8 +66,12 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+programmer_hash << :yukihiro_matsumoto => {
+				:known_for => "Ruby",
+					:languages => ["LISP", "C"]
+}
 
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -80,11 +87,11 @@ def adding_matz
         }
      }
 
-    
+
 end
 
 def changing_alan
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -99,14 +106,15 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-     alans_new_info = "GUI"
-     
-     
+		 programmer_hash[:alan_kay][:known_for] = "GUI"
+     #change what Alan Kay is :known_for to the value of the alans_new_info variable.
+     programmer_hash
+
+
 end
 
 def adding_to_dennis
-	programmer_hash = 
+	programmer_hash =
  		{
         :grace_hopper => {
           :known_for => "COBOL",
